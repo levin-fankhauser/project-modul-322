926500d987b4a0c9d7391cca6a8d83f8015771a4
levin-fankhauser <levin.fankhauser@gmx.ch>
2023-12-07 10:36:44 +0100

    Modify

--- src/main/java/org/vaadin/project/MainView.java
+++ src/main/java/org/vaadin/project/MainView.java
@@ -1,6 +1,5 @@
 package org.vaadin.project;
 
-import ch.qos.logback.core.Layout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
